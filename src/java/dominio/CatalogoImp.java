a24915d4fde2b5c6b7e840e4727535431ecc1d54
alexdz14 <zS22016132@estudiantes.uv.mx>
2025-11-05 18:12:09 -0600

    Clase 05/11/2025

--- src/java/dominio/CatalogoImp.java
+++ src/java/dominio/CatalogoImp.java
@@ -1,20 +1,23 @@
 package dominio;
 
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import modelo.mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import pojo.Rol;
 
 public class CatalogoImp {
-    
-    public static List<Rol> obtenerRoles(){
+
+    public static List<Rol> obtenerRoles() {
         List<Rol> roles = null;
         SqlSession conexionBD = MyBatisUtil.getSession();
-        if(conexionBD != null){
-            try{
+        if (conexionBD != null) {
+            try {
                 roles = conexionBD.selectList("catalogo.roles");
                 conexionBD.close();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
